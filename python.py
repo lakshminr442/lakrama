4eafdd70bcd32723e61e6ee408c722bb144e3d08
lakshminr442 <79299131+lakshminr442@users.noreply.github.com>
2021-04-28 19:34:40 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
-eck if a number is prime or not
+#check if a number is prime or not
 
 num = 29
 
@@ -10,16 +10,16 @@ flag = False
 
 # prime numbers are greater than 1
 if num > 1:
-        # check for factors
-            for i in range(2, num):
-                        if (num % i) == 0:
-                                        # if factor is found, set flag to True
-                                                    flag = True
-                                                                # break out of loop
-                                                                            break
-                                                                        
-                                                                        # check if flag is True
-                                                                        if flag:
-                                                                                print(num, "is not a prime number")
-                                                                            else:
-                                                                                    print(num, "is a prime number")a
+# check for factors
+for i in range(2, num):
+if (num % i) == 0:
+# if factor is found, set flag to True
+flag = True
+# break out of loop
+break
+         
+# check if flag is True
+if flag:
+print(num, "is not a prime number")
+else:
+print(num, "is a prime number")
